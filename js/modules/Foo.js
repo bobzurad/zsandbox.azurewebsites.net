1a4897c81be0bf4c49a46f3f70fd1f8069787eab
Bob Zurad <bob@zurad.net>
2014-07-28 17:24:45 -0500

    added jquery, underscore, requireJS and a test module

---
+++ js/modules/Foo.js
@@ -0,0 +1,18 @@
+define(
+  "Foo",
+  ["jquery"],
+  function($) {
+    "use strict";
+
+    //constructor
+    var Foo = function(params) {
+
+    }
+
+    Foo.prototype = {
+
+    };
+
+    return Foo;
+  }
+);
