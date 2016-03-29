6ff0837b700635b4816cd06c295b1d51f493c7bf
Bob Zurad <bob@zurad.net>
2016-03-28 22:43:54 -0500

    codeschool backbone 2

--- CodeSchool/Backbone2/lessons3-7.js
+++ CodeSchool/Backbone2/lessons3-7.js
@@ -17,3 +17,33 @@ var AppRouter = new (Backbone.Router.extend({
     console.log(appointment);
   }
 }))();
+
+//escaping user content, to protect from XSS attacks
+var AppointmentView = Backbone.View.extend({
+  template: _.template("<span><%= model.escape('title') %></span>"),
+  render: function(){
+    this.$el.html(this.template({model: this.model}));
+  }
+});
+
+//redirect after save
+var AppointmentForm = Backbone.View.extend({
+  template: _.template('<form><input name="title" type="text" value="<%= title %>" /><input name="name" type="text" value="<%= name %>" /></form>'),
+  render: function(){
+    this.$el.html(this.template(this.model.attributes));
+    return this;
+  },
+  events: {
+    submit: "save"
+  },
+  save: function(e){
+    e.preventDefault();
+    var newTitle = this.$('input[name=title]').val();
+    var newName = this.$('input[name=name]').val();
+    this.model.save({title: newTitle, name: newName}, {
+      success: function(model, response, options) {
+        Backbone.history.navigate('', { trigger: true });
+      }
+    });
+  }
+});
