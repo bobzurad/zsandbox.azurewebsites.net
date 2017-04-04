0d53d77d36fd52bb88ca922d1d977832472c0b53
Bob Zurad <bob@zurad.net>
2017-04-03 20:16:10 -0500

    tour of heroes - refactored routing into module, refactored master-detail page

--- angular2/tour-of-heroes/src/app/heroes.component.ts
+++ angular2/tour-of-heroes/src/app/heroes.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  constructor(private heroService: HeroService) { }
+
+  constructor(
+    private router: Router,
+    private heroService: HeroService) { }
 
   heroes = Hero[10];
   selectedHero: Hero;
@@ -27,4 +32,8 @@ export class HeroesComponent implements OnInit {
       .getHeroes()
       .then(heroes => this.heroes = heroes);
   }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }  
 }
