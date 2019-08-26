89b7ce61e9422c949b50107c4370ac3e2a0c320b
Bob Zurad <bob@zurad.net>
2019-08-25 22:00:08 -0500

    Mastering Vuex - State & Getters

--- VueMastery/mastering-vuex/src/store.js
+++ VueMastery/mastering-vuex/src/store.js
@@ -4,7 +4,46 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 'abc123', name: 'Adam Jahr' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    events: [
+      { id: 1, title: '...', organizer: '...' },
+      { id: 2, title: '...', organizer: '...' },
+      { id: 3, title: '...', organizer: '...' },
+      { id: 4, title: '...', organizer: '...' },
+      { id: 5, title: '...', organizer: '...' }
+    ],
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false },
+      { id: 3, text: '...', done: true },
+      { id: 4, text: '...', done: false }
+    ]
+  },
   mutations: {},
-  actions: {}
+  actions: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    },
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    },
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    // a getter that can access other getters
+    activeTodosCount: (state, getters) => {
+      return state.todos.length - getters.doneTodos.length
+    }
+  }
 })
--- VueMastery/mastering-vuex/src/views/EventCreate.vue
+++ VueMastery/mastering-vuex/src/views/EventCreate.vue
@@ -1,3 +1,47 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create Event, {{ userName }}</h1>
+    <p>This event was created by {{ userName }}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+    <p>{{ localComputed }}</p>
+    <p>There are {{ catLength }} items listed.</p>
+    <p>{{ getEventById(2) }}</p>
+  </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  /* the old way
+  computed: {
+    userName() {
+      return this.$store.state.user.name
+    }
+  }
+  */
+
+  //the new way
+  computed: {
+    localComputed() {
+      return 'i did not come from the store'
+    },
+    catLength() {
+      return this.$store.getters.catLength
+    },
+    //... is JavaScript's new object-spread operator https://github.com/tc39/proposal-object-rest-spread
+    ...mapGetters(['getEventById']),
+    ...mapState({
+      userName: state => state.user.name,
+      userId: state => state.user.id,
+      categories: 'categories' //can also do it this way for top level items in state
+    })
+  }
+
+  /* can also do it this way, mapping state directly
+  computed: mapState(['user', 'categories'])
+  */
+}
+</script>
