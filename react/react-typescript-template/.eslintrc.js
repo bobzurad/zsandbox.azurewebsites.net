77d52c3102bd88eaa5e33e41aa8f0901656ab460
Bob Zurad <bob.zurad@mimacom.com>
2020-09-10 23:59:16 -0500

    react-typescript template

---
+++ react/react-typescript-template/.eslintrc.js
@@ -0,0 +1,107 @@
+module.exports = {
+  "extends": [
+    "airbnb-typescript-prettier"
+  ],
+  "rules": {
+    "arrow-parens": [
+      "warn",
+      "as-needed"
+    ],
+    "comma-dangle": [
+      "warn",
+      "only-multiline"
+    ],
+    "camelcase": [
+      "warn",
+      {
+        "properties": "always"
+      }
+    ],
+    "curly": "warn",
+    "indent": [
+      "warn",
+      2
+    ],
+    "jsx-quotes": [
+      "warn",
+      "prefer-double"
+    ],
+    "max-len": [
+      "warn",
+      {
+        "code": 120,
+        "ignoreComments": true,
+        "ignoreUrls": true,
+        "ignoreStrings": true,
+        "tabWidth": 2
+      }
+    ],
+    "no-duplicate-imports": "warn",
+    "no-mixed-spaces-and-tabs": [
+      "warn",
+      "smart-tabs"
+    ],
+    "no-trailing-spaces": "warn",
+    "object-curly-spacing": [
+      "warn",
+      "always"
+    ],
+    "padded-blocks": [
+      "warn",
+      {
+        "blocks": "never",
+        "classes": "never"
+      },
+      {
+        "allowSingleLineBlocks": false
+      }
+    ],
+    "padding-line-between-statements": [
+      "warn",
+      {
+        "blankLine": "any",
+        "prev": "class",
+        "next": "*"
+      },
+      {
+        "blankLine": "always",
+        "prev": "*",
+        "next": "return"
+      }
+    ],
+    "quote-props": [
+      "warn",
+      "as-needed"
+    ],
+    "quotes": [
+      "warn",
+      "double",
+      {
+        "allowTemplateLiterals": true
+      }
+    ],
+    "react/jsx-closing-bracket-location": [
+      "warn",
+      "tag-aligned"
+    ],
+    "react/jsx-wrap-multilines": [
+      "warn",
+      {
+        "declaration": "parens-new-line",
+        "assignment": "parens-new-line",
+        "return": "parens-new-line",
+        "arrow": "parens-new-line",
+        "condition": "ignore",
+        "logical": "ignore",
+        "prop": "ignore"
+      }
+    ],
+    "semi": [
+      "warn",
+      "always"
+    ],
+    "space-before-blocks": "warn",
+    "space-before-function-paren": "warn",
+    "space-infix-ops": "warn"
+  }
+};
---
+++ react/react-typescript-template/README.md
@@ -0,0 +1,13 @@
+A react app made from scratch.
+
+Configured to use:
+
+* TypeScript
+* SASS
+* Image File Loading (png, svg, jpg, gif)
+* Jest
+* ESLint
+
+Created from (except for linting) https://medium.com/@tim.givois.mendez/create-a-react-project-from-scratch-without-create-react-app-f02fce4e05b
+
+For linting, I'm using ESLint with airbnb (and some rules that I personally prefer) from here: https://itnext.io/how-to-setup-typescript-eslint-prettier-and-react-in-5-minutes-44cfe8af5081
