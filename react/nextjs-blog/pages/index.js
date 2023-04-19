690d46aa08da6e873b9db41a99bc4e1b6dc2ea96
Bob Zurad <bob@zurad.net>
2023-04-18 22:12:36 -0500

    some data

--- react/nextjs-blog/pages/index.js
+++ react/nextjs-blog/pages/index.js
@@ -1,8 +1,22 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from "../components/layout";
+import { getSortedPostsData } from "../utils/posts";
 import utilStyles from "../styles/utils.module.css";
 
-export default function Home() {
+// getStaticProps can ONLY be used in pages. the code runs on the server.
+// data is fetched at build time!!
+// to fetch data at request time, use getServerSideProps() instead!!
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -14,6 +28,20 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
