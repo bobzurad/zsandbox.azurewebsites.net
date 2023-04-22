7a68efbac8ab265381fd98ac2291eed3d5c80568
Bob Zurad <bob@zurad.net>
2023-04-21 22:34:34 -0500

    removed tailwindcss

--- react/ecomapp-react/src/pages/index.tsx
+++ react/ecomapp-react/src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { Inter } from "next/font/google";
+import styles from "../styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,10 +12,10 @@ export default function Home() {
       <Head>
         <title>EcomApp</title>
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+      <main className={`${styles.main} ${inter.className}`}>
+        <div className={styles.center}>
           <Image
-            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+            className={styles.logo}
             src="/next.svg"
             alt="Next.js Logo"
             width={180}
@@ -22,6 +23,7 @@ export default function Home() {
             priority
           />
         </div>
+
         <Link href="/category/">Categories</Link>
       </main>
     </>
