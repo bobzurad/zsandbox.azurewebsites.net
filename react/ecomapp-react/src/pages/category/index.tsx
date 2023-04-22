970f62a088a49eda607b86b10a262d36dc1c10b8
Bob Zurad <bob@zurad.net>
2023-04-21 21:56:24 -0500

    some work on categories

--- react/ecomapp-react/src/pages/category/index.tsx
+++ react/ecomapp-react/src/pages/category/index.tsx
@@ -1,23 +1,62 @@
+import { GetServerSideProps } from "next";
+import Head from "next/head";
 import Link from "next/link";
 
-export default function Home() {
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  const request = await fetch("http://localhost:8080/category/");
+  const data = await request.json();
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+export default function Home({
+  data,
+}: {
+  data: {
+    id: number;
+    categoryName: string;
+    description: string;
+    imageUrl: string;
+  }[];
+}) {
+  console.log(data);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="columns-3 w-full gap-12">
-        <div>Categories</div>
-        <div>test</div>
-        <div>test2</div>
-      </div>
-      <div className="columns-3 w-full gap-12">
-        <div>Add Category</div>
-        <div>
-          <input />
+    <>
+      <Head>
+        <title>EcomApp - Categories</title>
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="columns-3 w-full gap-12">
+          <div>Categories</div>
+          {data.length === 0 ? (
+            <>
+              <div>There are no categories.</div>
+            </>
+          ) : (
+            <>
+              {data.forEach((category) => {
+                <>
+                  <div>{category.categoryName}</div>
+                  <div>{category.description}</div>
+                </>;
+              })}
+            </>
+          )}
         </div>
-        <div>
-          <button>Add</button>
+        <div className="columns-3 w-full gap-12">
+          <div>Add Category</div>
+          <div>
+            <input />
+          </div>
+          <div>
+            <button>Add</button>
+          </div>
         </div>
-      </div>
-      <Link href="/">← Back to Home</Link>
-    </main>
+        <Link href="/">← Back to Home</Link>
+      </main>
+    </>
   );
 }
