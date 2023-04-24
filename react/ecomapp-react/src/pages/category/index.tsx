8295792a2e8a03a543106f736db72f0b7ca64d9e
Bob Zurad <bob@zurad.net>
2023-04-23 23:17:41 -0500

    add category

--- react/ecomapp-react/src/pages/category/index.tsx
+++ react/ecomapp-react/src/pages/category/index.tsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import { GetServerSideProps } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import { Button, Input, Space, Form } from "antd";
+import { postData } from "@/util/api";
+import { validateMessages } from "@/util/form";
 import styles from "../../styles/category/Category.module.css";
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   const request = await fetch("http://localhost:8080/category/");
   const data = await request.json();
+
   return {
     props: {
       data,
@@ -23,7 +28,39 @@ export default function Home({
     imageUrl: string;
   }[];
 }) {
-  console.log(data);
+  const [categories, setCategories] = useState(data);
+  const [newCategoryName, setNewCategoryName] = useState("");
+  const [newCategoryDescription, setNewCategoryDescription] = useState("");
+  const [addLoading, setAddLoading] = useState(false);
+
+  const newCategoryFormLayout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  };
+
+  const newCategoryHandler = () => {
+    setAddLoading(true);
+
+    postData("http://localhost:8080/category/create/", {
+      categoryName: newCategoryName,
+      description: newCategoryDescription,
+      imageUrl: "https://picsum.photos/200",
+    })
+      .then(async (responseData) => {
+        console.log(responseData);
+        setNewCategoryName("");
+        setNewCategoryDescription("");
+        setAddLoading(false);
+        const request = await fetch("http://localhost:8080/category/");
+        const data = await request.json();
+        setCategories(data);
+      })
+      .catch((reason) => {
+        console.log(reason);
+        setAddLoading(false);
+      });
+  };
+
   return (
     <>
       <Head>
@@ -31,30 +68,69 @@ export default function Home({
       </Head>
       <main className={styles.main}>
         <div className={styles.center}>
-          <div>Categories</div>
-          {data.length === 0 ? (
-            <>
-              <div>There are no categories.</div>
-            </>
-          ) : (
-            <>
-              {data.forEach((category) => {
-                <>
-                  <div>{category.categoryName}</div>
-                  <div>{category.description}</div>
-                </>;
-              })}
-            </>
-          )}
+          <Space size="large">
+            <div>Categories</div>
+            {categories.length === 0 ? (
+              <>
+                <div>There are no categories.</div>
+              </>
+            ) : (
+              <>
+                {categories.map((category) => {
+                  return (
+                    <div key={category.id}>
+                      <div>{category.categoryName}</div>
+                      <div>{category.description}</div>
+                    </div>
+                  );
+                })}
+              </>
+            )}
+          </Space>
         </div>
         <div className={styles.center}>
-          <div>Add Category</div>
-          <div>
-            <input />
-          </div>
-          <div>
-            <button>Add</button>
-          </div>
+          <Form
+            {...newCategoryFormLayout}
+            name="add-category"
+            style={{ minWidth: 350 }}
+            validateMessages={validateMessages}
+            onFinish={newCategoryHandler}
+          >
+            <Form.Item
+              name="newCategoryName"
+              label="Name"
+              rules={[{ required: true }]}
+            >
+              <Input
+                size="large"
+                value={newCategoryName}
+                onChange={(event) =>
+                  setNewCategoryName(event.currentTarget.value)
+                }
+              />
+            </Form.Item>
+            <Form.Item name="newCategoryDescription" label="Description">
+              <Input.TextArea
+                size="large"
+                value={newCategoryDescription}
+                onChange={(event) =>
+                  setNewCategoryDescription(event.currentTarget.value)
+                }
+              />
+            </Form.Item>
+            <Form.Item
+              wrapperCol={{ ...newCategoryFormLayout.wrapperCol, offset: 8 }}
+            >
+              <Button
+                type="primary"
+                size="large"
+                htmlType="submit"
+                loading={addLoading}
+              >
+                Add Category
+              </Button>
+            </Form.Item>
+          </Form>
         </div>
         <Link href="/">← Back to Home</Link>
       </main>
