309f1507e5e0f3ffb25d58e9afd2efbc22508adb
Bob Zurad <bob@zurad.net>
2023-04-25 18:00:26 -0500

    moved AddCategory to it's own component

--- react/ecomapp-react/src/components/category/addCategory.tsx
+++ react/ecomapp-react/src/components/category/addCategory.tsx
@@ -1,1 +1,94 @@
-export default function AddCategory() {}
+import { useState, useRef } from "react";
+import Head from "next/head";
+import { Button, Input, Form } from "antd";
+import type { FormInstance } from "antd";
+import { postData } from "@/util/api";
+import { validateMessages } from "@/util/form";
+
+export default function AddCategory() {
+  const [newCategoryName, setNewCategoryName] = useState("");
+  const [newCategoryDescription, setNewCategoryDescription] = useState("");
+  const [newCategoryIsLoading, setNewCategoryIsLoading] = useState(false);
+
+  const newCategoryFormRef = useRef<FormInstance>(null);
+
+  const newCategoryFormLayout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  };
+
+  const newCategoryHandler = () => {
+    setNewCategoryIsLoading(true);
+
+    postData("http://localhost:8080/category/create/", {
+      categoryName: newCategoryName,
+      description: newCategoryDescription,
+      imageUrl: "https://picsum.photos/200",
+    })
+      .then(async (responseData) => {
+        // on successful post, reset form and react state
+        newCategoryFormRef.current?.resetFields();
+        setNewCategoryName("");
+        setNewCategoryDescription("");
+        setNewCategoryIsLoading(false);
+        // refresh categories
+        const request = await fetch("http://localhost:8080/category/");
+        const data = await request.json();
+        console.log(data);
+        //updateCategories(data);
+      })
+      .catch((reason) => {
+        console.log(reason);
+        setNewCategoryIsLoading(false);
+      });
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Add Category</title>
+      </Head>
+      <Form
+        {...newCategoryFormLayout}
+        name="add-category"
+        ref={newCategoryFormRef}
+        style={{ minWidth: 500 }}
+        validateMessages={validateMessages}
+        onFinish={newCategoryHandler}
+      >
+        <Form.Item
+          name="newCategoryName"
+          label="Name"
+          rules={[{ required: true }]}
+        >
+          <Input
+            size="large"
+            value={newCategoryName}
+            onChange={(event) => setNewCategoryName(event.currentTarget.value)}
+          />
+        </Form.Item>
+        <Form.Item name="newCategoryDescription" label="Description">
+          <Input.TextArea
+            size="large"
+            value={newCategoryDescription}
+            onChange={(event) =>
+              setNewCategoryDescription(event.currentTarget.value)
+            }
+          />
+        </Form.Item>
+        <Form.Item
+          wrapperCol={{ ...newCategoryFormLayout.wrapperCol, offset: 8 }}
+        >
+          <Button
+            type="primary"
+            size="large"
+            htmlType="submit"
+            loading={newCategoryIsLoading}
+          >
+            Add Category
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
+}
--- react/ecomapp-react/src/pages/category/index.tsx
+++ react/ecomapp-react/src/pages/category/index.tsx
@@ -1,12 +1,10 @@
-import { useState, useRef, useContext, useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { GetServerSideProps } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import { Button, Input, Space, Form } from "antd";
-import type { FormInstance } from "antd";
+import { Space } from "antd";
 import { CategoryContext, CategoryContextType } from "@/context/category";
-import { postData } from "@/util/api";
-import { validateMessages } from "@/util/form";
+import AddCategory from "../../components/category/addCategory";
 import styles from "../../styles/category/Category.module.css";
 import { ICategoryModel } from "@/models/category";
 
@@ -30,42 +28,6 @@ export default function Home({ data }: { data: ICategoryModel[] }) {
     updateCategories(data);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data]);
-
-  const [newCategoryName, setNewCategoryName] = useState("");
-  const [newCategoryDescription, setNewCategoryDescription] = useState("");
-  const [newCategoryIsLoading, setNewCategoryIsLoading] = useState(false);
-
-  const newCategoryFormRef = useRef<FormInstance>(null);
-
-  const newCategoryFormLayout = {
-    labelCol: { span: 8 },
-    wrapperCol: { span: 16 },
-  };
-
-  const newCategoryHandler = () => {
-    setNewCategoryIsLoading(true);
-
-    postData("http://localhost:8080/category/create/", {
-      categoryName: newCategoryName,
-      description: newCategoryDescription,
-      imageUrl: "https://picsum.photos/200",
-    })
-      .then(async (responseData) => {
-        // on successful post, reset form and react state
-        newCategoryFormRef.current?.resetFields();
-        setNewCategoryName("");
-        setNewCategoryDescription("");
-        setNewCategoryIsLoading(false);
-        // refresh categories
-        const request = await fetch("http://localhost:8080/category/");
-        const data = await request.json();
-        updateCategories(data);
-      })
-      .catch((reason) => {
-        console.log(reason);
-        setNewCategoryIsLoading(false);
-      });
-  };
 
   return (
     <>
@@ -95,49 +57,7 @@ export default function Home({ data }: { data: ICategoryModel[] }) {
           </Space>
         </div>
         <div className={styles.center}>
-          <Form
-            {...newCategoryFormLayout}
-            name="add-category"
-            ref={newCategoryFormRef}
-            style={{ minWidth: 500 }}
-            validateMessages={validateMessages}
-            onFinish={newCategoryHandler}
-          >
-            <Form.Item
-              name="newCategoryName"
-              label="Name"
-              rules={[{ required: true }]}
-            >
-              <Input
-                size="large"
-                value={newCategoryName}
-                onChange={(event) =>
-                  setNewCategoryName(event.currentTarget.value)
-                }
-              />
-            </Form.Item>
-            <Form.Item name="newCategoryDescription" label="Description">
-              <Input.TextArea
-                size="large"
-                value={newCategoryDescription}
-                onChange={(event) =>
-                  setNewCategoryDescription(event.currentTarget.value)
-                }
-              />
-            </Form.Item>
-            <Form.Item
-              wrapperCol={{ ...newCategoryFormLayout.wrapperCol, offset: 8 }}
-            >
-              <Button
-                type="primary"
-                size="large"
-                htmlType="submit"
-                loading={newCategoryIsLoading}
-              >
-                Add Category
-              </Button>
-            </Form.Item>
-          </Form>
+          <AddCategory />
         </div>
         <Link href="/">← Back to Home</Link>
       </main>
