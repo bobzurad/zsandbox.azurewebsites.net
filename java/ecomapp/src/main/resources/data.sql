2a6db87840bf87775089022b3c9916d22538a395
Bob Zurad <bob@zurad.net>
2023-05-04 16:42:12 -0500

    added some seed data

---
+++ java/ecomapp/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO categories (category_name, description, image_url) VALUES
+    ('Fruit', 'Here are some fruits.', 'https://picsum.photos/200'),
+    ('Vegetable', 'Veggies!', 'https://picsum.photos/200'),
+    ('Dairy', 'moo!', 'https://picsum.photos/200');
