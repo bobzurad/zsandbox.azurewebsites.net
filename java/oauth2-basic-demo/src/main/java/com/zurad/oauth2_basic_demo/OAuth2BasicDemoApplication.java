48a9dfdaffed72f6f77e211d742b0aca5663b6e9
Bob Zurad <bob@zurad.net>
2025-01-10 09:39:52 -0600

    basic oauth2 in spring boot

---
+++ java/oauth2-basic-demo/src/main/java/com/zurad/oauth2_basic_demo/OAuth2BasicDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zurad.oauth2_basic_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2BasicDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2BasicDemoApplication.class, args);
+	}
+
+}
---
+++ java/oauth2-basic-demo/src/test/java/com/zurad/oauth2_basic_demo/OAuth2BasicDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zurad.oauth2_basic_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2BasicDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
