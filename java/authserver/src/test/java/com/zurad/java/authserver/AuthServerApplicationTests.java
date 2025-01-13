57b9059b3384d656aa0380eb9551d73c6b0c2b2b
Bob Zurad <bob@zurad.net>
2025-01-13 10:50:25 -0600

    initial authserver

---
+++ java/authserver/src/test/java/com/zurad/java/authserver/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zurad.java.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
