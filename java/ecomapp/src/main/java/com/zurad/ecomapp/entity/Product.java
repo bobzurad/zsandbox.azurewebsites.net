a0666f8cf7aeb30ff0948b43c2ae34fe1cf7d8fa
Bob Zurad <bob@zurad.net>
2025-11-10 23:36:42 -0600

    data for products

--- java/ecomapp/src/main/java/com/zurad/ecomapp/entity/Product.java
+++ java/ecomapp/src/main/java/com/zurad/ecomapp/entity/Product.java
@@ -1,7 +1,6 @@
 package com.zurad.ecomapp.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 
@@ -9,78 +8,79 @@ import jakarta.validation.constraints.NotNull;
 @Table(name = "products")
 public class Product {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
 
-    private @NotNull String name;
-    private @NotNull String imageURL;
-    private @NotNull double price;
-    private @NotNull String description;
+  private @NotNull String name;
+  private @NotNull String imageUrl;
+  private @NotNull double price;
+  private @NotNull String description;
 
-    @JsonIgnore
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "category_id", nullable = false)
-    Category category;
+  @JsonIgnore
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(name = "category_id", nullable = false)
+  Category category;
 
-    public Product() {}
+  public Product() {}
 
-    public Product(String name, String imageURL, double price, String description, Category category) {
-        super();
-        this.name = name;
-        this.imageURL = imageURL;
-        this.price = price;
-        this.description = description;
-        this.category = category;
-    }
+  public Product(
+      String name, String imageUrl, double price, String description, Category category) {
+    super();
+    this.name = name;
+    this.imageUrl = imageUrl;
+    this.price = price;
+    this.description = description;
+    this.category = category;
+  }
 
-    // getters and setters
+  // getters and setters
 
-    public Integer getId() {
-        return id;
-    }
+  public Integer getId() {
+    return id;
+  }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+  public void setId(Integer id) {
+    this.id = id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public String getImageURL() {
-        return imageURL;
-    }
+  public String getimageUrl() {
+    return imageUrl;
+  }
 
-    public void setImageURL(String imageURL) {
-        this.imageURL = imageURL;
-    }
+  public void setimageUrl(String imageUrl) {
+    this.imageUrl = imageUrl;
+  }
 
-    public double getPrice() {
-        return price;
-    }
+  public double getPrice() {
+    return price;
+  }
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
+  public void setPrice(double price) {
+    this.price = price;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public String getDescription() {
+    return description;
+  }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  public void setDescription(String description) {
+    this.description = description;
+  }
 
-    public Category getCategory() {
-        return category;
-    }
+  public Category getCategory() {
+    return category;
+  }
 
-    public void setCategory(Category category) {
-        this.category = category;
-    }
+  public void setCategory(Category category) {
+    this.category = category;
+  }
 }
--- java/ecomapp/src/main/resources/data.sql
+++ java/ecomapp/src/main/resources/data.sql
@@ -2,3 +2,8 @@ INSERT INTO categories (category_name, description, image_url) VALUES
     ('Fruit', 'Here are some fruits.', 'https://picsum.photos/200'),
     ('Vegetable', 'Veggies!', 'https://picsum.photos/200'),
     ('Dairy', 'moo!', 'https://picsum.photos/200');
+
+INSERT INTO products (name, category_id, description, price, image_url) VALUES
+    ('Pear', 1, 'Better than an Apple', 0.45, 'https://picsum.photos/200'),
+    ('Squash', 2, 'Better than an egg plant', 0.12, 'https://picsum.photos/200'),
+    ('Carrot', 2, 'For rabbits', 0.23, 'https://picsum.photos/200');
